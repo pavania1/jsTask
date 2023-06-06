1f2acbce7ad56367cb4e74b0f2cdf1b14a51b045
likhith <lucien302@protonmail.com>
2023-06-06 23:25:28 +0530

    Initial Tasks

--- createpost/script.js
+++ createpost/script.js
@@ -1,5 +1,5 @@
 // script.js
-document.addEventListener('DOMContentLoaded', getAllPosts);
+document.addEventListener('DOMContentLoaded', loadData);
 
 function performGetRequest(url, callback) {
   var xhr = new XMLHttpRequest();
@@ -14,34 +14,41 @@ function performGetRequest(url, callback) {
   };
   xhr.send();
 }
-
-function getAllPosts() {
+var Posts;
+function loadData() {
   var url = 'https://jsonplaceholder.typicode.com/posts';
   performGetRequest(url, function(error, responseData) {
     if (error) {
       console.error(error);
     } else {
-      displayPosts(responseData);
+       Posts=responseData;
+      getAllPosts();      
     }
   });
 }
+function getAllPosts() {
+ displayPosts(Posts);
+}
 
 function getPostById() {
-  var postIdInput = document.getElementById('postIdInput');
-  var postId = postIdInput.value;
-  var url = 'https://jsonplaceholder.typicode.com/posts/' + postId;
-  performGetRequest(url, function(error, responseData) {
-    if (error) {
-      console.error(error);
-    } else {
-      if (responseData.id) {
-        displayPosts([responseData]);
+  let postIdInput = document.getElementById('postIdInput');
+  let postId = postIdInput.value;
+  console.log(typeof postId);
+  // var url = 'https://jsonplaceholder.typicode.com/posts/' + postId;
+  // performGetRequest(url, function(error, responseData) {
+    // if (error) {
+      // console.error(error);
+    // } else {
+      // if (responseData.id) {
+      if (Posts.filter(x=>x.id===parseInt(postId)).length>0) {
+        // console.log(Posts[postId-1]);
+        displayPosts(Posts.filter(x=>x.id===parseInt(postId)));
       } else {
         console.log('Post not found.');
       }
     }
-  });
-}
+  // });
+// }
 
 function displayPosts(posts) {
   var tableBody = document.querySelector('#postsTable tbody');
@@ -71,6 +78,10 @@ function showCreatePostForm() {
   var createPostFormContainer = document.getElementById('createPostFormContainer');
   createPostFormContainer.style.display = 'block';
 }
+function showUpdatePostForm() {
+  var createPostFormContainer = document.getElementById('updatePostFormContainer');
+  createPostFormContainer.style.display = 'block';
+}
 
 function createPost(event) {
   event.preventDefault();
@@ -80,27 +91,61 @@ function createPost(event) {
   var title = document.getElementById('title').value;
   var body = document.getElementById('body').value;
 
-  var post = {
+  let post = {
     id: parseInt(postId),
     userId: parseInt(userId),
     title: title,
     body: body
   };
+  Posts.push(post);
+  Posts.sort((a,b)=>(a.id-b.id));
+  getAllPosts();
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
+  // xhr.setRequestHeader('Content-Type', 'application/json');
+  // xhr.onload = function() {
+  //   if (xhr.status === 201) {
+    //     var createdPost = JSON.parse(xhr.responseText);
+  //     console.log('Created Post:', createdPost);
+  //     displayNewPost(createdPost); // Display the new post in the table
+  //     resetCreatePostForm();
+  //   } else {
+  //     console.error('Error: ' + xhr.status);
+  //   }
+  // };
+  // xhr.send(JSON.stringify(post));
+}
+function updatePost(event) {
+  event.preventDefault();
+  delById();
+  var postId = document.getElementById('postIdInput').value;
+  var userId = document.getElementById('userId').value;
+  var title = document.getElementById('title').value;
+  var body = document.getElementById('body').value;
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-    if (xhr.status === 201) {
-      var createdPost = JSON.parse(xhr.responseText);
-      console.log('Created Post:', createdPost);
-      displayNewPost(createdPost); // Display the new post in the table
-      resetCreatePostForm();
-    } else {
-      console.error('Error: ' + xhr.status);
-    }
+  let post = {
+    id: parseInt(postId),
+    userId: parseInt(userId),
+    title: title,
+    body: body
   };
-  xhr.send(JSON.stringify(post));
+  Posts.push(post);
+  Posts.sort((a,b)=>(a.id-b.id));
+  getAllPosts();
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts', true);
+  // xhr.setRequestHeader('Content-Type', 'application/json');
+  // xhr.onload = function() {
+  //   if (xhr.status === 201) {
+  //     var createdPost = JSON.parse(xhr.responseText);
+  //     console.log('Created Post:', createdPost);
+  //     displayNewPost(createdPost); // Display the new post in the table
+  //     resetCreatePostForm();
+  //   } else {
+  //     console.error('Error: ' + xhr.status);
+  //   }
+  // };
+  // xhr.send(JSON.stringify(post));
 }
 
 function displayNewPost(post) {
@@ -128,3 +173,26 @@ function resetCreatePostForm() {
   var createPostForm = document.getElementById('createPostForm');
   createPostForm.reset();
 }
+const clearTable=()=>{
+  Posts=[];
+  displayPosts();
+}
+function delById() {
+  let postIdInput = document.getElementById('postIdInput');
+  let postId = postIdInput.value;
+  console.log(typeof postId);
+  // var url = 'https://jsonplaceholder.typicode.com/posts/' + postId;
+  // performGetRequest(url, function(error, responseData) {
+    // if (error) {
+      // console.error(error);
+    // } else {
+      // if (responseData.id) {
+      if (Posts.filter(x=>x.id===parseInt(postId)).length>0) {
+        // console.log(Posts[postId-1]);
+        displayPosts(Posts=Posts.filter(x=>x.id!==parseInt(postId)));
+      } else {
+        console.log('Post not found to be deleted.');
+      }
+    }
+  // });
+// }
